8b008e9417a4c9c2c69dd620f9535747aee5960b
felipe-frade <39383884+felipe-frade@users.noreply.github.com>
2021-09-21 23:58:56 -0300

    atualizando

--- resources/views/contato/index.blade.php
+++ resources/views/contato/index.blade.php
@@ -1,6 +1,14 @@
+@extends('layout.site')
+
+@section('titulo', 'Contatos')
+
+@section('conteudo')
+
 <h3>Essa é a view index</h3>
 
 @foreach($lista as $contato)
     <p>{{ $contato["nome"] }}</p>
     <p>{{ $contato["telefone"] }}</p>
 @endforeach
+
+@endsection('conteudo')
